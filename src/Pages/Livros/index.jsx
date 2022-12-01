8cdb4f5257d3458b00ac1bcac4b5022fb8e21e72
CMCCVR <cmccvr@gmail.com>
2022-12-01 18:36:23 -0300

    capas de livros

--- src/Pages/Livros/index.jsx
+++ src/Pages/Livros/index.jsx
@@ -1,25 +1,34 @@
 import React from "react";
-import { ContainerPage, TitlePage } from '../../Components/Main';
+import { ContainerPage, TitlePage } from "../../Components/Main";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Livros = () => {
-const [livros, setLivros] = useState([])
+const [livros, setLivros] = useState([]); 
 
-useEffect(() => {
-  axios.get("http://localhost:5000/books")
-    .then((response) => {
-    console.log(response.data)
-    setLivros(response.data)
-})
-},[]);
+  // LET !!!!!! ou CONST !!!!!  linha 6
 
-    return (
-      <ContainerPage>
-        <TitlePage> Livros </TitlePage> 
-        {JSON.stringify(livros)}       
-      </ContainerPage>  
-   );
-}
+  useEffect(() => {
+    axios.get("http://localhost:5000/books").then((response) => {
+      console.log(response.data);
+      setLivros(response.data);
+    });
+  }, []);
+
+  return (
+    <ContainerPage>
+      <TitlePage> Livros </TitlePage>
+      {livros?.map((livro) => (
+        <div>
+          <span>{livro._id}</span>{" "}
+          <span>
+            {livro.title} <img src={`./img/${livro._id}.jpg`} alt="img" />
+            {livro.title} <img src={`./img/${livro._id}.jpg`} alt="img" />
+          </span>
+        </div>
+      ))}
+    </ContainerPage>
+  );
+};
 
 export default Livros;
